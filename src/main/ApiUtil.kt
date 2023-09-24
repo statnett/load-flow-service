7e8399068b352d52f7b61403eba447eb7024ef0b
David Kleiven <davidkleiven446@gmail.com>
2023-09-24 21:24:30 +0200

    Refactor: Put file content extraction into ApiUtil

--- src/main/ApiUtil.kt
+++ src/main/ApiUtil.kt
@@ -1,5 +1,6 @@
 package com.github.statnett.loadflowservice
 
+import io.ktor.http.content.*
 import java.io.ByteArrayInputStream
 
 fun busesFromRequest(
@@ -8,4 +9,23 @@ fun busesFromRequest(
 ): List<BusProperties> {
     val network = networkFromStream(type, ByteArrayInputStream(body))
     return busPropertiesFromNetwork(network)
+}
+
+class FileContent(val name: String, val bytes: ByteArray)
+
+suspend fun fileContentFromRequest(multiPartData: MultiPartData): FileContent {
+    var name = ""
+    var content = byteArrayOf()
+    multiPartData.forEachPart { part ->
+        when (part) {
+            is PartData.FileItem -> {
+                name = part.originalFileName as String
+                content = part.streamProvider().readBytes()
+            }
+
+            else -> {}
+        }
+        part.dispose()
+    }
+    return FileContent(name, content)
 }
