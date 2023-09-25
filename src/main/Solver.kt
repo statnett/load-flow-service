7336816afc8422b82a562dacd3d4028ad679518f
David Kleiven <david.kleiven@statnett.no>
2023-09-25 12:25:41 +0200

    Add function that returns default load parameters as json

--- src/main/Solver.kt
+++ src/main/Solver.kt
@@ -3,6 +3,8 @@ package com.github.statnett.loadflowservice
 import com.powsybl.iidm.network.Network
 import com.powsybl.loadflow.LoadFlow
 import com.powsybl.loadflow.LoadFlowParameters
+import com.powsybl.loadflow.json.JsonLoadFlowParameters
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
 
 fun networkFromStream(
@@ -12,9 +14,20 @@ fun networkFromStream(
     return Network.read(fname, content)
 }
 
+fun defaultLoadFlowParameters(): String {
+    val parameters = LoadFlowParameters()
+    val stream = ByteArrayOutputStream()
+    JsonLoadFlowParameters.write(parameters, stream)
+
+    // TODO: here we undo pretty printing. See if a raw JSON string can be obtained in a more elegant way
+    return undoPrettyPrintJson(stream.toString())
+}
+
 fun solve(
     network: Network,
     parameters: LoadFlowParameters,
 ) {
     LoadFlow.run(network, parameters)
 }
+
+
