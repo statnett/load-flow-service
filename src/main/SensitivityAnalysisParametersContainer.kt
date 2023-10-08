8dbd66fc538a10dba336a855dc38955d096d8595
David Kleiven <davidkleiven446@gmail.com>
2023-10-08 14:42:51 +0200

    Add SensitivityAnalysisParameters container

---
+++ src/main/SensitivityAnalysisParametersContainer.kt
@@ -0,0 +1,30 @@
+package com.github.statnett.loadflowservice
+
+
+import com.powsybl.sensitivity.SensitivityAnalysisParameters
+import com.powsybl.sensitivity.json.JsonSensitivityAnalysisParameters
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.content.*
+
+private val logger = KotlinLogging.logger {}
+
+class SensitivityAnalysisParametersContainer : AutoVersionableJsonParser(), FormItemLoadable {
+    var parameters = SensitivityAnalysisParameters()
+
+    override fun currentVersion(): String {
+        return SensitivityAnalysisParameters.VERSION
+    }
+
+    fun update(jsonString: String) {
+        val withVersion = jsonStringWithVersion(jsonString)
+        this.parameters = JsonSensitivityAnalysisParameters.update(this.parameters, withVersion.byteInputStream())
+    }
+
+    override fun formItemHandler(part: PartData.FormItem) {
+        val name = part.name ?: ""
+        if (name == "sensitivity-analysis-parameters") {
+            this.update(part.value)
+            logger.info { "Received load flow parameters: ${part.value}" }
+        }
+    }
+}
